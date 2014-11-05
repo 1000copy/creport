233ce80ff875c8aae52313f6ae4de68f5d7cba4e
liu chuan jun <1000copy@gmail.com>
2014-11-05 15:21:30 +0800

    -

--- demo/REportDemo.dfm
+++ demo/REportDemo.dfm
@@ -18,32 +18,17 @@ object CReportDemoForm: TCReportDemoForm
   PixelsPerInch = 96
   TextHeight = 13
   object SpeedButton1: TSpeedButton
-    Left = 296
-    Top = 216
+    Left = 568
+    Top = 144
     Width = 89
     Height = 24
     Caption = #25171#21360'DbGrid1'
-    OnClick = SpeedButton1Click
-  end
-  object SpeedButton3: TSpeedButton
-    Left = 416
-    Top = 216
-    Width = 105
-    Height = 24
-    Caption = #21160#24577#25253#34920#31034#20363
-    Font.Charset = GB2312_CHARSET
-    Font.Color = clWindowText
-    Font.Height = -14
-    Font.Name = #26999#20307'_GB2312'
-    Font.Style = []
-    ParentFont = False
-    OnClick = SpeedButton3Click
   end
   object DBGrid1: TDBGrid
     Left = 40
     Top = 56
     Width = 513
-    Height = 153
+    Height = 321
     Color = clWhite
     DataSource = Dataform.DataSource1
     ImeName = #20116#31508#30011
@@ -151,6 +136,28 @@ object CReportDemoForm: TCReportDemoForm
     Align = alRight
     BevelOuter = bvNone
     TabOrder = 3
+    object SpeedButton2: TSpeedButton
+      Left = 5
+      Top = 88
+      Width = 89
+      Height = 22
+      Caption = #25171#21360'DbGrid2'
+      OnClick = SpeedButton2Click
+    end
+    object SpeedButton3: TSpeedButton
+      Left = -3
+      Top = 216
+      Width = 105
+      Height = 24
+      Caption = #21160#24577#25253#34920#31034#20363
+      Font.Charset = GB2312_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -14
+      Font.Name = #26999#20307'_GB2312'
+      Font.Style = []
+      ParentFont = False
+      OnClick = SpeedButton3Click
+    end
     object Button3: TButton
       Left = 10
       Top = 45
@@ -171,7 +178,7 @@ object CReportDemoForm: TCReportDemoForm
     end
     object Button4: TButton
       Left = 10
-      Top = 138
+      Top = 290
       Width = 83
       Height = 32
       Caption = #20851#38381
@@ -211,69 +218,6 @@ object CReportDemoForm: TCReportDemoForm
     Align = alBottom
     BevelOuter = bvNone
     TabOrder = 4
-    object SpeedButton2: TSpeedButton
-      Left = 288
-      Top = 0
-      Width = 89
-      Height = 22
-      Caption = #25171#21360'DbGrid2'
-      OnClick = SpeedButton2Click
-    end
-    object Button2: TButton
-      Left = 42
-      Top = 0
-      Width = 231
-      Height = 25
-      Caption = #22635'('#20462#25913')'#24403#21069#35760#24405#22270#29255'(table2'#34920')'
-      Font.Charset = GB2312_CHARSET
-      Font.Color = clWindowText
-      Font.Height = -14
-      Font.Name = #26999#20307'_GB2312'
-      Font.Style = []
-      ParentFont = False
-      TabOrder = 0
-      OnClick = Button2Click
-    end
-  end
-  object DBGrid2: TDBGrid
-    Left = 40
-    Top = 248
-    Width = 513
-    Height = 129
-    DataSource = Dataform.DataSource2
-    TabOrder = 5
-    TitleFont.Charset = DEFAULT_CHARSET
-    TitleFont.Color = clWindowText
-    TitleFont.Height = -11
-    TitleFont.Name = 'MS Sans Serif'
-    TitleFont.Style = []
-    Columns = <
-      item
-        Expanded = False
-        FieldName = 'Nn'
-        Visible = True
-      end
-      item
-        Expanded = False
-        FieldName = 'Loel'
-        Title.Caption = #22270#29255
-        Visible = True
-      end>
-  end
-  object Button1: TButton
-    Left = 50
-    Top = 216
-    Width = 223
-    Height = 25
-    Caption = #22635'('#20462#25913')'#24403#21069#35760#24405#22270#29255'(table1'#34920')'
-    Font.Charset = GB2312_CHARSET
-    Font.Color = clWindowText
-    Font.Height = -14
-    Font.Name = #26999#20307'_GB2312'
-    Font.Style = []
-    ParentFont = False
-    TabOrder = 6
-    OnClick = Button1Click
   end
   object ReportRunTime1: TReportRunTime
     EnableEdit = True
--- demo/REportDemo.pas
+++ demo/REportDemo.pas
@@ -20,23 +20,17 @@ type
     Button4: TButton;
     Button5: TButton;
     opbm1: TOpenPictureDialog;
-    DBGrid2: TDBGrid;
-    Button1: TButton;
-    Button2: TButton;
     SpeedButton1: TSpeedButton;
-    SpeedButton2: TSpeedButton;
     CheckBox1: TCheckBox;
-    SpeedButton3: TSpeedButton;
     ReportControl1: TReportControl;
     SaveDialog1: TSaveDialog;
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
     procedure Button4Click(Sender: TObject);
     //procedure Button3Click(Sender: TObject);
     procedure Button5Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure Button1Click(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
-    procedure SpeedButton1Click(Sender: TObject);
     procedure SpeedButton2Click(Sender: TObject);
     procedure CheckBox1Click(Sender: TObject);
     procedure FormPaint(Sender: TObject);
@@ -98,77 +92,14 @@ dataform.table2.open;
 
 end;
 
-procedure TCReportDemoForm.Button1Click(Sender: TObject);
-var Fpicture:Tpicture;
-    FBmp:TBitmap;
-begin
-  if opbm1.Execute then
-  begin
-    Fpicture:=Tpicture.Create;
-    Fpicture.LoadFromFile(opbm1.filename);
-    fbmp:=TBitmap.Create;
-    if not (Fpicture.Graphic is Ticon) then
-      fbmp.Assign(Fpicture.Graphic)
-    else
-    begin
-      fbmp.Width := Fpicture.Icon.Width;
-      fbmp.Height := Fpicture.Icon.Height;
-      fbmp.Canvas.Draw(0, 0, Fpicture.Icon );
-    end;
-    dataform.Table1.Edit;
-    dataform.Table1.FieldByName('bm').Assign(fbmp);
-    dataform.Table1.post;
-    Fpicture.Free;
-    fbmp.Free;
-  end;
-end;
 
-procedure TCReportDemoForm.Button2Click(Sender: TObject);
-var Fpicture:Tpicture;
-    FBmp:TBitmap;
-begin
-  if opbm1.Execute then
-  begin
-    Fpicture:=Tpicture.Create;
-    Fpicture.LoadFromFile(opbm1.filename);
-    fbmp:=TBitmap.Create;
-    if not (Fpicture.Graphic is Ticon) then
-      fbmp.Assign(Fpicture.Graphic)
-    else
-    begin
-      fbmp.Width := Fpicture.Icon.Width;
-      fbmp.Height := Fpicture.Icon.Height;
-      fbmp.Canvas.Draw(0, 0, Fpicture.Icon );
-    end;
-    dataform.Table2.Edit;
-    dataform.Table2.FieldByName('loel').Assign(fbmp);
-    dataform.Table2.post;
-    Fpicture.Free;
-    fbmp.Free;
-  end;
-end;
 
-procedure TCReportDemoForm.SpeedButton1Click(Sender: TObject);
-//var i:integer;
-//    l1,l2:Tstringlist;
-begin
-{
-l1:=Tstringlist.Create;
-l2:=Tstringlist.Create;
-for i:=0 to dbgrid1.Columns.Count -1 do
-begin
-  l1.Add(dbgrid1.Columns[i].Title.caption);
-  l2.Add(dbgrid1.Columns[i].fieldname);
-end;
-}
-  ReportRunTime1.prdbgrid:=dbgrid1;  //ֻ֧?ִ?TDbGrid?????? dbgrid
-  ReportRunTime1.previewDbGrid(self.Name);
-end;
+
 
 procedure TCReportDemoForm.SpeedButton2Click(Sender: TObject);
 begin
-ReportRunTime1.prdbgrid:=dbgrid2;
-ReportRunTime1.previewDbGrid(self.name);
+  ReportRunTime1.prdbgrid:=dbgrid1;  //ֻ֧?ִ?TDbGrid?????? dbgrid
+  ReportRunTime1.previewDbGrid(self.Name);
 
 end;
 
@@ -193,101 +124,97 @@ var j:integer;
     CellFont: TLogFont;
     cf: TFont;
 begin
-   strFileDir := ExtractFileDir(Application.ExeName);
-with  ReportControl1 do
-begin
-   SetWndSize(1058,748);// ????ֽ?Ŵ?С
+ 		strFileDir := ExtractFileDir(Application.ExeName);
+		with  ReportControl1 do
+		begin
+			SetWndSize(1058,748); 
+			NewTable(dbgrid1.Columns.Count ,6);
 
-   NewTable(dbgrid1.Columns.Count ,6);// ????????
+			SetCellSFocus(0,0,0,dbgrid1.Columns.Count-1);
+			CombineCell;
+			setLineHegit(0,80);
+			SetCellLines(false,false,false,false,1,1,1,1);
+			SetCallText(0,0,'֧Ʊ');
+			SetCellAlign(1, 1);
 
-   SetCellSFocus(0,0,0,dbgrid1.Columns.Count-1);//ѡȡ0??
-   CombineCell;   //?ϲ?0?е?Ԫ??
-   setLineHegit(0,80); //?趨0?еĸ߶?
-   SetCellLines(false,false,false,false,1,1,1,1); //ȥ????????
-   SetCallText(0,0,'??00??????????֧ͳ?Ʊ?');  //??0?е?????
-   SetCellAlign(1, 1);//??ѡ???е????־???
+			cf := Tfont.Create;
+			cf.Name := '????_GB2312';
+			cf.Size := 22;
+			cf.style :=cf.style+ [fsBold];
 
-   cf := Tfont.Create;
-   cf.Name := '????_GB2312';
-   cf.Size := 22;
-   cf.style :=cf.style+ [fsBold];
-   //cf.style:=cf.style+ [fsItalic];//б??
-   //cf.style:=cf.style+ [fsunderline];//?»???
-   GetObject(cf.Handle, SizeOf(CellFont), @CellFont);
-   SetCellFont(CellFont); //?趨????
+			GetObject(cf.Handle, SizeOf(CellFont), @CellFont);
+			SetCellFont(CellFont);
 
-   for j:=0 to dbgrid1.Columns.Count -1 do //???ֶ???????ϸ???ֶ?
-   begin
-     SetCallText(1,j,dbgrid1.Columns[j].FieldName);
-     SetCallText(2,j,'#T1.'+dbgrid1.Columns[j].FieldName);
+			for j:=0 to dbgrid1.Columns.Count -1 do
+			begin
+			 SetCallText(1,j,dbgrid1.Columns[j].FieldName);
+			 SetCallText(2,j,'#T1.'+dbgrid1.Columns[j].FieldName);
 
-     RemoveAllSelectedCell;
-     SetCellFocus(2,j);//
+			 RemoveAllSelectedCell;
+			 SetCellFocus(2,j);//
 
-     if dbgrid1.DataSource.DataSet.FieldByName(dbgrid1.Columns[j].FieldName) is tnumericField then
-     begin
-       SetCellAlign(2, 1);
-       SetCellDispFormt('0,.00');
-    end
-     else
-       SetCellAlign(3, 1);
+			 if dbgrid1.DataSource.DataSet.FieldByName(dbgrid1.Columns[j].FieldName) is tnumericField then
+			 begin
+			   SetCellAlign(2, 1);
+			   SetCellDispFormt('0,.00');
+			 end
+			 else
+			   SetCellAlign(3, 1);
+			end;
+			setLineHegit(1,40);  
 
-   end;
-   setLineHegit(1,40); //?趨??2?еĸ߶?
+			RemoveAllSelectedCell;
+			SetCellSFocus(1,0,1,dbgrid1.Columns.Count-1);
+			SetCellAlign(1, 1);
 
-   RemoveAllSelectedCell;//ȡ??ѡ?е?Ԫ??
-   SetCellSFocus(1,0,1,dbgrid1.Columns.Count-1);//ѡ????1?н??в???
-   SetCellAlign(1, 1);//??ѡ???е????־???
+			cf.Name := '????_GB2312';
+			cf.Size := 16;
+			cf.Style:=[];
+			GetObject(cf.Handle, SizeOf(CellFont), @CellFont);
 
-   cf.Name := '????_GB2312';
-   cf.Size := 16;
-   cf.Style:=[];
-   GetObject(cf.Handle, SizeOf(CellFont), @CellFont);
+			SetCellFont(CellFont);
+			SetCellColor(clRed, clWhite);
 
-   SetCellFont(CellFont);
-   SetCellColor(clRed, clWhite);  //?׵׺???
+			SetCallText(3,0,'֧Ʊ');
+			SetCallText(3,3,'`SumAll(4)');
 
-   SetCallText(3,0,'?ϼ?');
-   SetCallText(3,3,'`SumAll(4)'); //ȷ??Ҫȡ?ϼ????ĵ?Ԫ??
-
-   RemoveAllSelectedCell;
-   SetCellFocus(3,3);//ѡ????1?н??в???
-   SetCellAlign(2, 1);
-   SetCellDispFormt('0,.00');
+			RemoveAllSelectedCell;
+			SetCellFocus(3,3);
+			SetCellAlign(2, 1);
+			SetCellDispFormt('0,.00');
 
 
-   RemoveAllSelectedCell;
-   SetCellSFocus(4,0,4,dbgrid1.Columns.Count-1); //ѡ??????һ??
-   SetCellLines(false,false,false,false,1,1,1,1); //ȥ????????
-   CombineCell;                                   //?ϲ???Ԫ??
-   SetCallText(4,0,'`PageNum/');                  //????????Ϊ"???/?ҳ"??ʽ??ҳ??
-   SetCellAlign(1, 1);                            //????
+			RemoveAllSelectedCell;
+			SetCellSFocus(4,0,4,dbgrid1.Columns.Count-1);
+			SetCellLines(false,false,false,false,1,1,1,1);
+			CombineCell;
+			SetCallText(4,0,'`PageNum/');
+			SetCellAlign(1, 1);
 
-   RemoveAllSelectedCell;//ȡ??ѡ?е?Ԫ??
-   SetCellSFocus(1,0,3,dbgrid1.Columns.Count-1);//ѡ????1?н??в???
-   SetCellFocus(4,0);//ѡ????1?н??в???
-   cf.Name := 'MS Serif';
-   cf.Size :=10;
-   cf.Style:=[];
-   GetObject(cf.Handle, SizeOf(CellFont), @CellFont);
-   SetCellFont(CellFont);
+			RemoveAllSelectedCell;
+			SetCellSFocus(1,0,3,dbgrid1.Columns.Count-1);
+			SetCellFocus(4,0);
+			cf.Name := 'MS Serif';
+			cf.Size :=10;
+			cf.Style:=[];
+			GetObject(cf.Handle, SizeOf(CellFont), @CellFont);
+			SetCellFont(CellFont);
 
-   RemoveAllSelectedCell;//ȡ??ѡ?е?Ԫ??
-   SetCellSFocus(5,0,5,dbgrid1.Columns.Count-1);//ѡ????1?н??в???
-   SetCellLines(false,false,false,false,1,1,1,1); //ȥ????????
-   CombineCell;                                   //?ϲ???Ԫ??
-   SetCallText(5,0,'@T2.Loel');                  //????????Ϊ"???/?ҳ"??ʽ??ҳ??
-   setLineHegit(5,250); //?趨??5?еĸ߶?
+			RemoveAllSelectedCell;
+			SetCellSFocus(5,0,5,dbgrid1.Columns.Count-1);
+			SetCellLines(false,false,false,false,1,1,1,1);
+			CombineCell;
+			SetCallText(5,0,'@T2.Loel');
+			setLineHegit(5,250);
 
-   SaveToFile(strFileDir+'\'+'xxx.ept');
-   ResetContent;
-   cf.Free;
-end;
-   //Ԥ??
-   dataform.Table1.DisableControls;
-   ReportRunTime1.ReportFile:=strFileDir+'\'+'xxx.ept';
-   ReportRunTime1.PrintPreview(true); //true??????ʾԤ??????ʾ?Ǵ?ӡ??????
-   dataform.Table1.EnableControls;
+			SaveToFile(strFileDir+'\'+'xxx.ept');
+			ResetContent;
+			cf.Free;
+		end;
 
+		dataform.Table1.DisableControls;
+		ReportRunTime1.ReportFile:=strFileDir+'\'+'xxx.ept';
+		ReportRunTime1.PrintPreview(true); 
+		dataform.Table1.EnableControls;
 end;
 end.
