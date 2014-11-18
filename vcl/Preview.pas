928be0edfeed577d5ae6943b3b49c50c5a993bd3
liu chuan jun <1000copy@gmail.com>
2014-11-18 17:15:46 +0800

    -

--- vcl/Preview.pas
+++ vcl/Preview.pas
@@ -366,10 +366,8 @@ end;
 procedure TPreviewForm.DoFit();
 
 begin
-    ReportControl1.FreeEdit; // add ¿Ó‘Û¬◊
-   zoomxxx := ReportControl1.ZoomRate(Height,Width,110,170,cp_pgh,cp_pgw);
-
-
+  ReportControl1.FreeEdit; // add ¿Ó‘Û¬◊
+  zoomxxx := ReportControl1.ZoomRate(Height,Width,110,170,cp_pgh,cp_pgw);
   ShowWindow(ReportControl1.Handle, SW_HIDE);
   ReportControl1.ReportScale := zoomxxx;
   ScrollBox1Resize(Self);
