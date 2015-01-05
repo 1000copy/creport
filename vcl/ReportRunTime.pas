b407497891221c8fad241438536a673b6c227e46
liu chuan jun <1000copy@gmail.com>
2015-01-05 13:40:02 +0800

    -

--- vcl/ReportRunTime.pas
+++ vcl/ReportRunTime.pas
@@ -57,7 +57,7 @@ type
     FPrintLineList: TList;
     FDRMap: TDRMappings;
     FNamedDatasets: TDataList;
-    FHeaderHeight: Integer;         //
+    FHeaderHeight: Integer;         
     Fallprint: Boolean;
     FPageCount: Integer;
     FDataLineHeight: Integer;
